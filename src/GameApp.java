89cdeec37fdd1d662207dda0afa5730b7f54611a
Brooke <brookeydai@gmail.com>
2019-08-22 20:53:39 -0700

    converted to maven project

---
+++ src/GameApp.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.*;
+
+public class GameApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+
+}
